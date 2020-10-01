6f264512a597c7698cacf656ed4870fcee1df6ab
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-01 11:53:58 -0600

    Setting up form fields and labels for the operands in calc.php

--- calc.php
+++ calc.php
@@ -9,9 +9,29 @@ include './templates/header.php';
 This is the Calculator Page...!
 </p>
 
-<?php // Show the Footer
-include './templates/footer.php';
+<form>
+    <label for="num1">
+    Enter First Operand:
+    <input 
+    id="num1" 
+    name="value1"
+    type="number"
+    value="">
+    </label>
 
-?>
+    <label for="num2">
+    Enter Second Operand:
+    <input 
+    id="num2" 
+    name="value2"
+    type="number"
+    value="">
+    </label>
+</form>
+
+
+
+<?php // Show the Footer
+include './templates/footer.php';?>
 
 <h3>Hi there!</h3>
