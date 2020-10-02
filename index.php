5b498ce8194d26373d1bd1504173e802f985b5a0
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-02 10:38:11 -0600

    Added Calculator history

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php 
+session_start();
+
 // Global Variables are stored in PHP's $GLOBALS array...
 $GLOBALS['pageTitle'] = 'Home';
 
@@ -7,8 +9,24 @@ include './templates/header.php';
 ?>
 
 <p> 
-    This is the Home Page!
+    This is the 
+    
+    <?php echo $GLOBALS['pageTitle']; ?> 
+    Page!
 </p>
+
+
+<?php if ( isset( $_SESSION['calcHistory'] ) ) : ?>
+    <h2>Calculator History from $_SESSION</h2>
+    <ul>
+        <?php foreach ($_SESSION['calcHistory'] as $calculation) : ?>
+            <li>
+            <?php echo $calculation; ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
 
 <?php // Show the Footer
 include './templates/footer.php';
