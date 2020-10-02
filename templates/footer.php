453c227cf948612a02823bd741780045913c3511
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-02 17:15:13 -0600

    Cleaned up header and footer pages

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,6 @@
 <footer>
     <h2>Footer Details</h2>
-    <p>
-        &copy;
-        <?php echo date( 'Y' ); ?>
-        TECHCareers
-    </p>
+    <p> &copy; <?php echo date( 'Y' ); ?> TECHCareers </p>
 </footer>
     
 </body>
--- templates/header.php
+++ templates/header.php
@@ -4,10 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $GLOBALS['pageTitle']; ?></title>
-
-    <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="css/main.css">
-    <!-- Scripts -->
     <script type="text/JavaScript" src="js/scripts.js" defer></script>
 
 </head>
@@ -15,6 +12,7 @@
     <h1>
     <?php echo $GLOBALS['pageTitle']; ?>
     </h1>
-    
-    <?php // dirname( __FILE__ ) grabs the currently executing file's absolute path (as a string)
-    include dirname( __FILE__ ).'/navigation.php'; ?>
+    <?php include dirname( __FILE__ ).'/navigation.php'; ?>
+
+<?php // Continues to Body, then Footer... 
+?>
