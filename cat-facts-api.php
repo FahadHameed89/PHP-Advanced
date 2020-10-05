94fbeec3b880bcea7f1265301f29fc1abbde87c7
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-05 11:06:30 -0600

    Added working example of API call using Daily-Cat-Facts API

--- cat-facts-api.php
+++ cat-facts-api.php
@@ -1,11 +1,19 @@
 <?php 
 $GLOBALS['pageTitle'] = 'Cat Facts (External API Test)';
 include './templates/header.php';
+$dailyCatFactResponse = file_get_contents( 'https://cat-fact.herokuapp.com/facts/random' ); // Source Link: https://alexwohlbruck.github.io/cat-facts/docs/
+// Test output in var_dump
+// var_dump ( $dailyCatFactResponse );
+
+// If we get a response...run 
+if ( $dailyCatFactResponse ) {
+    $dailyCatFact = json_decode( $dailyCatFactResponse );
 ?>
-
-<h3>Welcome to the Cat Facts Page...!</h3>
-
-
+    <h3>Welcome to the Cat Facts Page...!</h3>
+    <p><?php echo $dailyCatFact->text; ?></p>
+<?php 
+}
+?>
 
 <?php // Show the Footer
 include './templates/footer.php';
