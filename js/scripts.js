edc5c8e5d9ab4321f8d517ff7ceb9f2b87d215ed
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-05 14:30:21 -0600

    Set up a functioning query parameter search function for the JSON data

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// Test that this file is loaded properly!
+// alert( 'I\'m here!' );
+
+/**
+ * Retrieve the form, input, and ul.
+ */
+const snackSearchForm = document.getElementById( 'search-form' );
+const snackSearchInput = document.getElementById( 'search-input' );
+const searchResultsList = document.getElementById( 'search-results' );
+
+/**
+ * Listen for a submit.
+ */
+snackSearchForm.addEventListener( 'submit', event => {
+  // Stop the form from submitting the traditional way.
+  event.preventDefault();
+  // Attempt a fetch for results.
+  fetch ( `http://localhost:3000/api/snacks.php?search=${snackSearchInput.value}` )
+    .then( response => response.json() )
+    .then( data => {
+      console.log( data );
+    } )
+} );
