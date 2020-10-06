ee750c3555a6f043bb7a5019bc2a008524256159
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-06 10:13:09 -0600

    Added SearchForm component, nested in example js

--- js/react-example.js
+++ js/react-example.js
@@ -3,4 +3,28 @@
 // Get the react root element defined in HTML
 const reactRoot = document.getElementById( 'react-root' );
 
-ReactDOM.render ( <p>Hello, Bruv!</p>, reactRoot );
+// Search Form Component
+const SearchForm = props => {
+    return (
+        <div>
+        <h2>Snack Search Form</h2>
+        <form>
+            <label htmlFor="search">
+                Enter a Search Term:
+                <input
+                    type="search"
+                    id="search"
+                />
+            </label>
+            <input 
+                type="submit"
+                value="Search Snacks" 
+            />
+        </form>
+        <h3>Snack Search Results</h3>
+        <ul></ul>
+        </div>
+    );
+}
+
+ReactDOM.render ( <SearchForm />, reactRoot );
